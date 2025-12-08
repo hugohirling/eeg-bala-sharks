5fd1c4c6839cac3631ac9963e0435fc3507d062d
hugohirling <hugo.hirling@web.de>
2025-12-08 14:40:30 +0100

    Changed config

--- eeg_pipeline/config.py
+++ eeg_pipeline/config.py
@@ -15,17 +15,17 @@ SUBJECTS = ["01"]
 FREQ_LOWER = 1.0
 FREQ_UPPER = 40.0
 
+#Downsampling
+DOWNSAMPLE_SFREQ = 512
+
 # Epoching
-EPOCH_DURATION = 1.0
 MAX_EPOCHS = 500
+EPOCH_TMIN = -0.2
+EPOCH_TMAX = 5.0
 
 # AutoReject config
 AR_N_JOBS = 1       # Number of parallel jobs
 AR_VERBOSE = True   # Verbose output
-MAX_EPOCHS = 500    # Max number of epochs AutoReject uses
-EPOCH_DURATION = 2  # seconds
-FREQ_LOWER = 1.0    # Hz
-FREQ_UPPER = 40.0   # Hz
 
 # ICA
 ICA_N_COMPONENTS = 20
